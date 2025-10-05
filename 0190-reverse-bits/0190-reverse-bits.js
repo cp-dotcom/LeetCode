674cabaa9f1c092a617cb952b3170677a9223a25
cp-dotcom <cmdvishnucp2018@gmail.com>
2025-10-05 20:04:55 +0530

    Time: 61 ms (8.03%), Space: 56.3 MB (9.44%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function(n) {
+    return Number.parseInt(n.toString(2)
+    .split("").reverse().join("")
+    .padEnd(32,"0"),2)
+};
